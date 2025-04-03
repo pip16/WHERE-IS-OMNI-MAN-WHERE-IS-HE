1acf68d497225a99d79f5a5cb2353388beee7c57
pip16 <maks2016.pulkin2006@gmail.com>
2025-04-03 23:07:31 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-
+            //3 Задание
+            
             //int number = int.Parse(Console.ReadLine());
             //int numbr = int.Parse(Console.ReadLine());
             //Random rna = new Random();
@@ -16,6 +17,8 @@ namespace ConsoleApp3
             //Console.WriteLine("конвертируемые числа");
             //Console.WriteLine(rna.Next(numbr, number));
 
+            //6 Задание
+            
             //float a = 3.2f;
             //decimal b = 2.6m;
             //double c = 4.5;
@@ -25,54 +28,77 @@ namespace ConsoleApp3
             //int r = d + f + g;
             //Console.WriteLine(r);
 
+            //7 Задание 
+            
             //int num = 3;
             //double num2 = 2.6;
             //Console.WriteLine(num + num2);
 
+            //8 Задание
+            
             //int A = int.Parse(Console.ReadLine());
             //Console.WriteLine(A * A);
 
+            //9 Задание
+            
             //Double B = Double.Parse(Console.ReadLine());
             //double a = Math.Round(B, 3);
             //Console.WriteLine(a);
 
+            //10 Задание
+            
             //double e = double.Parse(Console.ReadLine());
             //double blabla = Math.Round(e, 1);
             //Console.WriteLine(blabla);
 
+            //11 Задание
+            
             //int numbr = int.Parse(Console.ReadLine());
             //Console.Clear();
             //Console.WriteLine("Вы ввели число");
             //Console.WriteLine(numbr);
 
+            //12 Задание
+            
             //int numer = int.Parse(Console.ReadLine());
             //Console.Clear();
             //Console.WriteLine(numer);
             //Console.WriteLine("Вот какое число вы ввели");
 
+            //13 Задание
+            
             //Console.WriteLine("1 13 49");
             //Console.WriteLine("7  15  100");
 
+            //14 Задание
+            
             //float z; int a = 1, b = 2, c = 3;
             //const int m = 3;
             //z = (a + b + c) / m;
             //Console.WriteLine(z);
 
+            //15 Задание
+            
             //double a = int.Parse(Console.ReadLine());
             //double b = int.Parse(Console.ReadLine());
             //double c = (a + b) / 2;
             //Console.WriteLine(c);
 
+            //16 Задание
+            
             //int x = int.Parse(Console.ReadLine());
             //int n = int.Parse(Console.ReadLine());
             //double c = double.RootN(x, n);
             //Console.WriteLine(c);
 
+            //17 Задание
+            
             //int v = int.Parse(Console.ReadLine());
             //int p = int.Parse(Console.ReadLine());
             //int m = p * v;
             //Console.WriteLine(m);
 
+            //18 Задание
             //double a = double.Parse(Console.ReadLine());
             //double b = double.Parse(Console.ReadLine());
             //if (a != 0)
@@ -87,6 +113,8 @@ namespace ConsoleApp3
             //    Console.ReadKey(); 
             //}
 
+            //19 Задание
+            
             //double a = double.Parse(Console.ReadLine());
             //double b = double.Parse(Console.ReadLine());
             //double c = double.Parse(Console.ReadLine());
@@ -100,23 +128,29 @@ namespace ConsoleApp3
             //Console.WriteLine(disc);
             //Console.WriteLine(z);
 
+            //20 Задание
+            
             //double a = double.Parse(Console.ReadLine());
             //double b = double.Parse(Console.ReadLine());
             //double y = double.Parse(Console.ReadLine());
             //double c = Math.Pow(a, y) + Math.Pow(b, y);
             //Console.WriteLine(c);
 
+            //21 Задание
             //double R = double.Parse(Console.ReadLine());
             //double r = double.Parse(Console.ReadLine());
             //double S = 3.14*(Math.Pow(R,2) - Math.Pow(r,2));
             //Console.WriteLine(S);
 
+            //22 Задание
             //double a = double.Parse(Console.ReadLine());
             //double b = double.Parse(Console.ReadLine());
             //double c = double.Parse(Console.ReadLine());
             //double P = a + b + c;
             //Console.WriteLine(P);
 
+            //23 Задание
+            
             //double a = double.Parse(Console.ReadLine());
             //double b = double.Parse(Console.ReadLine());
             //double arif = (a + b) / 2;
@@ -126,11 +160,14 @@ namespace ConsoleApp3
             //Console.WriteLine(minus_geom);
             //Console.WriteLine(minus_arif);
 
+            //24 Задание
+            
             //int a = int.Parse(Console.ReadLine()); 
             //a = a * a; 
             //a = a * a; 
             //Console.WriteLine(a);
 
+            
             //int a = int.Parse(Console.ReadLine()); 
             //a = a * a; 
             //int a2 = a; 
@@ -178,6 +215,19 @@ namespace ConsoleApp3
             //mult *= a;
             //Console.WriteLine($"СуМмА: {sum}");
             //Console.WriteLine($"УмНоЖеНиЕ: {mult}");
+
+            int a, b, c;
+            a = b = c = 2;
+            double z_a = a * b + c++ / b - a * b * --c; //-3,66
+            double z_b = a * b + c++ / b - a * b * --c; //-1
+            double z_v = a * b + c++ / (b - a * b) * --c; //2
+            double z_g = a * (b + c++ / b) - a * b * --c; //-2
+            double z_d = a * (b + c++) / b - a * b * --c; //-4
+            Console.WriteLine(z_a);
+            Console.WriteLine(z_b);
+            Console.WriteLine(z_v);
+            Console.WriteLine(z_g);
+            Console.WriteLine(z_d);
 
         }
     }
