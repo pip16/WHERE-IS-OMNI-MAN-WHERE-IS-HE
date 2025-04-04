b5014c72a638f4e0492a430bd4c4d50282ab75e8
pip16 <maks2016.pulkin2006@gmail.com>
2025-04-04 12:30:20 +0400

    Add files via upload

---
+++ Практика 3.cs
@@ -0,0 +1,177 @@
+﻿namespace Практика_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1 (a)
+
+            //Console.Write("Задание а) ");
+            //Console.Write("Введите значение A (true/false): ");
+            //bool A1 = bool.Parse(Console.ReadLine());
+            //Console.Write("Введите значение B (true/false): ");
+            //bool B1 = bool.Parse(Console.ReadLine());
+            //bool C1 = !(!A1 && !B1) && A1;
+            //Console.WriteLine($"не (не {A1} и не {B1}) и {A1} = {C1} ");
+            //Console.ReadKey();
+
+            ////(б)
+
+            //Console.Write("Задание б) ");
+            //Console.Write("Введите значение a (true/false): ");
+            //bool A2 = bool.Parse(Console.ReadLine());
+            //Console.Write("Введите значение b (true/false): ");
+            //bool B2 = bool.Parse(Console.ReadLine());
+            //bool C2 = !(!A2 || !B2) || A2;
+            //Console.WriteLine($"не (не {A2} или не {B2}) или {A2} = {C2} ");
+            //Console.ReadKey();
+
+            ////(в)
+
+            //Console.Write("Задание в) ");
+            //Console.Write("Введите значение A (true/false): ");
+            //bool A3 = bool.Parse(Console.ReadLine());
+            //Console.Write("Введите значение B (true/false): ");
+            //bool B3 = bool.Parse(Console.ReadLine());
+            //bool C3 = !(!A3 || !B3) & A3;
+            //Console.WriteLine($"не (не {A3} или не {B3}) и {A3} = {C3} ");
+            //Console.ReadKey();
+
+            //Задание 2 а)
+
+            //Console.Write("Введите значение A (true/false): ");
+            //bool A = bool.Parse(Console.ReadLine());
+            //Console.Write("Введите значение B (true/false): ");
+            //bool B = bool.Parse(Console.ReadLine());
+            //Console.Write("Введите значение C (true/false): ");
+            //bool C = bool.Parse(Console.ReadLine());
+            //bool Res = !(A || !B && C);
+            //Console.WriteLine($"не ({A} или не {B} и {C} = {Res} ");
+
+            ////Задание 2 б)
+
+            //Console.Write("Введите значение A (true/false): ");
+            //bool A1 = bool.Parse(Console.ReadLine());
+            //Console.Write("Введите значение B (true/false): ");
+            //bool B1 = bool.Parse(Console.ReadLine());
+            //Console.Write("Введите значение C (true/false): ");
+            //bool C1 = bool.Parse(Console.ReadLine());
+            //bool Res1 = A1 && !(B1 && A1 || !C1);
+            //Console.WriteLine($"{A1} и не ({B1} и {A1} или {C1}) = {Res1} ");
+
+            ////Задание 2 в)
+
+            //Console.Write("Введите значение A (true/false): ");
+            //bool A2 = bool.Parse(Console.ReadLine());
+            //Console.Write("Введите значение B (true/false): ");
+            //bool B2 = bool.Parse(Console.ReadLine());
+            //Console.Write("Введите значение C (true/false): ");
+            //bool C2 = bool.Parse(Console.ReadLine());
+            //bool Res2 = !(!A2 || B2 && C2);
+            //Console.WriteLine($"не (не {A2} или {B2} и {C2} = {Res2} ");
+
+            ////Задание 2 г)
+
+            //Console.Write("Введите значение A (true/false): ");
+            //bool A3 = bool.Parse(Console.ReadLine());
+            //Console.Write("Введите значение B (true/false): ");
+            //bool B3 = bool.Parse(Console.ReadLine());
+            //Console.Write("Введите значение C (true/false): ");
+            //bool C3 = bool.Parse(Console.ReadLine());
+            //bool Res3 = !(A3 || !B3 && C3) || C3;
+            //Console.WriteLine($"не ({A3} или не {B3} и {C3}) или {C3} = {Res3} ");
+
+            ////Задание 2 д)
+
+            //Console.Write("Введите значение A (true/false): ");
+            //bool A4 = bool.Parse(Console.ReadLine());
+            //Console.Write("Введите значение B (true/false): ");
+            //bool B4 = bool.Parse(Console.ReadLine());
+            //Console.Write("Введите значение C (true/false): ");
+            //bool C4 = bool.Parse(Console.ReadLine());
+            //bool Res4 = !(A4 && !B4 || C4) && B;
+            //Console.WriteLine($"не ({A4} или не {B4} или {C4}) и {B4} = {Res4} ");
+
+            ////Задание 2 е)
+
+            //Console.Write("Введите значение A (true/false): ");
+            //bool A5 = bool.Parse(Console.ReadLine());
+            //Console.Write("Введите значение B (true/false): ");
+            //bool B5 = bool.Parse(Console.ReadLine());
+            //Console.Write("Введите значение C (true/false): ");
+            //bool C5 = bool.Parse(Console.ReadLine());
+            //bool Res5 = !(!A5 || B5 && C5) || A5;
+            //Console.WriteLine($"не (не {A5} или {B5} и {C5}) или {A5} = {Res5} ");
+
+            ////Задание 3 а)
+            //Console.Write("Введи значение x (true/false): ");
+            //bool x = bool.Parse(Console.ReadLine());
+            //Console.Write("Введи значение y (true/false): ");
+            //bool y = bool.Parse(Console.ReadLine());
+            //bool res = 
+            //Console.WriteLine($"({x}  )");
+            ////Задание 3 б)
+
+            //Console.Write("Введи значение x (true/false): ");
+            //bool x = bool.Parse(Console.ReadLine());
+            //Console.Write("Введи значение y (true/false): ");
+            //bool y = bool.Parse(Console.ReadLine());
+            //bool res =
+            //Console.WriteLine($"({x}  )");
+
+            ////Задание 3 в)
+
+            //Console.Write("Введи значение x (true/false): ");
+            //bool x = bool.Parse(Console.ReadLine());
+            //Console.Write("Введи значение y (true/false): ");
+            //bool y = bool.Parse(Console.ReadLine());
+            //bool res =
+            //Console.WriteLine($"({x}  )");
+
+            ////Задание 3 г)
+
+            //Console.Write("Введи значение x (true/false): ");
+            //bool x = bool.Parse(Console.ReadLine());
+            //Console.Write("Введи значение y (true/false): ");
+            //bool y = bool.Parse(Console.ReadLine());
+            //bool res =
+            //Console.WriteLine($"({x}  )");
+
+            ////Задание 3 д)
+
+            //Console.Write("Введи значение x (true/false): ");
+            //bool x = bool.Parse(Console.ReadLine());
+            //Console.Write("Введи значение y (true/false): ");
+            //bool y = bool.Parse(Console.ReadLine());
+            //bool res =
+            //Console.WriteLine($"({x}  )");
+
+            ////Задание 3 е)
+
+            //Console.Write("Введи значение x (true/false): ");
+            //bool x = bool.Parse(Console.ReadLine());
+            //Console.Write("Введи значение y (true/false): ");
+            //bool y = bool.Parse(Console.ReadLine());
+            //bool res =
+            //Console.WriteLine($"({x}  )");
+
+            ////Задание 3 ж)
+
+            //Console.Write("Введи значение x (true/false): ");
+            //bool x = bool.Parse(Console.ReadLine());
+            //Console.Write("Введи значение y (true/false): ");
+            //bool y = bool.Parse(Console.ReadLine());
+            //bool res =
+            //Console.WriteLine($"({x}  )");
+
+            ////Задание 3 з)
+
+            //Console.Write("Введи значение x (true/false): ");
+            //bool x = bool.Parse(Console.ReadLine());
+            //Console.Write("Введи значение y (true/false): ");
+            //bool y = bool.Parse(Console.ReadLine());
+            //bool res =
+            //Console.WriteLine($"({x}  )");
+        }
+    }
+}
