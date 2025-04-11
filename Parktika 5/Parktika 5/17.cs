ff142060f75c5e018fa766cfc79bdfce04f1719e
pip16 <maks2016.pulkin2006@gmail.com>
2025-04-11 12:44:40 +0400

    Add files via upload

---
+++ Parktika 5/Parktika 5/17.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parktika_5
+{
+    internal class _17
+    {
+        static void MidGrade(double eng, double rus, double deut, double math, double PE, double chemestry, double physic, double history, double geography, double biology)
+        {
+            double n = 10;
+
+
+        }
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
