cf62110e854f3e0ef97a7fb874408d9c49af70a9
pip16 <maks2016.pulkin2006@gmail.com>
2025-04-12 17:44:12 +0400

    Update 17.cs

--- Parktika 5/Parktika 5/17.cs
+++ Parktika 5/Parktika 5/17.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Parktika_5
-{
-    internal class _17
-    {
-        static void MidGrade(double eng, double rus, double deut, double math, double PE, double chemestry, double physic, double history, double geography, double biology)
-        {
-            double n = 10;
-
-
-        }
-        public static void Main(string[] args)
-        {
-
-        }
-    }
-}
+//namespace Praktika5
+//{
+//    internal class _15
+//    {
+//        static void Grades(int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8, int x9, int x10) 
+//        {
+//            int sum;
+//            sum = (x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10) / 10;
+//            Console.Write(sum);
+//        }
+//        public static void Main(string[] args)
+//        {
+//            Grades(4, 5, 2, 3, 4, 5, 2, 2, 3, 4);
+//        }
+//    }
+//}
