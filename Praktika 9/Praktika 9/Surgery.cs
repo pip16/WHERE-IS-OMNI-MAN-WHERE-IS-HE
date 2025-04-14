62f1603600501ad7e27298766daccec7b9a428db
pip16 <maks2016.pulkin2006@gmail.com>
2025-04-14 12:59:47 +0400

    Update Surgery.cs

--- Praktika 9/Praktika 9/Surgery.cs
+++ Praktika 9/Praktika 9/Surgery.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,10 @@ namespace Praktika_9
 {
     internal class Surgery
     {
+        public readonly int departmentid;
+        public string name;
+        public Cardiology headOfDepartment;
+        public List<Doctor> doctors;
+        public bool[] beds;
     }
 }
