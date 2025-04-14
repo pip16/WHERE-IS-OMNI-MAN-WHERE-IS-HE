5fcc58b28d5bd5247318deba6a57ccb42312ac32
pip16 <maks2016.pulkin2006@gmail.com>
2025-04-14 12:57:40 +0400

    Add files via upload

---
+++ Praktika 9/Praktika 9/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika_9
+{
+    internal class Patient
+    {
+        public readonly int patiendid;
+        public string FirstName;
+        public string LastName;
+        public DateTime dateOfBirth;
+        public string diagnosis;
+
+        public Patient
+    }
+}
