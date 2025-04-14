8f97d5a8abaab87c9cc0e2d673806db0b990802b
pip16 <maks2016.pulkin2006@gmail.com>
2025-04-14 12:59:15 +0400

    Update Hospital.cs

--- Praktika 9/Praktika 9/Hospital.cs
+++ Praktika 9/Praktika 9/Hospital.cs
@@ -6,31 +6,15 @@ using System.Threading.Tasks;
 
 namespace Praktika_9
 {
-    internal class Patient
+    internal class Hospital
     {
-        public readonly int patientid;
-        public string FirstName;
-        public string LastName;
-        public DateTime dateOfBirth;
-        public string diagnosis;
-
-        public Patient () { }
-        public Patient (int patientid) {this.patientid = patientid; }
-        public Patient (int patientid, string FirstName) :this (patientid) {this .FirstName = FirstName; }
-        public Patient (int patientid, string FirstName, string LastName) :this (patientid, FirstName) {this .LastName = LastName; }
-        public Patient (int patientid, string FirstName, string LastName, DateTime dateOfBirth) :this (patientid, FirstName, LastName) { this.dateOfBirth = dateOfBirth; }
-
-        public void UpdateDiagnosis(string NewDiagnosis)
-        {
-            this.diagnosis = NewDiagnosis;
-        }
-        public void GetAge(DateTime dateofBirth)
-        {
-            
-        }
-        public void ToString()
-        {
-
-        }
+        public readonly int hospitalid;
+        public string name;
+        public string address;
+        public List<string> departments;
+        public Hospital() { }
+        public Hospital(int hospitalid) { this.hospitalid = hospitalid; }
+        public Hospital(int hospitalid, string name) : this(hospitalid) { this.name = name; }
+        public Hospital(int hospitalid, string name, string address) : this(hospitalid, name) { this.address = address; }        
     }
 }
